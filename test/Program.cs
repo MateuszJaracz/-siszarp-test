8312a352f901214898ca0df78ee98686b5b005ac
Mateusz Jaracz <mateuszjaracz@twocode.pl>
2019-04-09 13:24:58 +0200

    Dodaj pliki projektów.

---
+++ test/Program.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OpenQA.Selenium.IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("http://google.pl");
+            //ChromeWebElement element = driver.FindElement(DivideByZeroException.
+            //OpenQA.Selenium.IWebDriver element = driver.FindElement
+
+            OpenQA.Selenium.IWebElement elementName = driver.FindElement(by: nameof("q"));
+
+            OpenQA.Selenium.IWebElement element = driver.FindElement(by:class("g"));
+
+          
+       
+
+            elementName.SendKeys("executeautomation");
+
+            driver.Close();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private static By nameof(string v)
+        {
+           throw new NotImplementedException();
+       }
+    }
+}
